eb75dd070ae84c1a926ccdcdb926370507d195dc
Scott Howland <thydungeonmaster@gmail.com>
2016-07-26 14:33:15 -0700

    Node names; Display methods
    Corrected non-correspondence between nokogiri node names and their xml counterparts. Removed unnecessary display methods.

--- lib/peak_list_parser.rb
+++ lib/peak_list_parser.rb
@@ -9,7 +9,7 @@ module Bruker
       # ARGS
       #    ds : The raw contents of the "PeakList1DHeader" tag
       # RETURNS
-      #    A new PickDetails object built from ds
+      #    A new PeakPickDetails object built from ds
       def parseDetails(ds)
         c = ds.content
         f1 = /F1=(.*)ppm, F2/.match(c)[1]
@@ -17,37 +17,37 @@ module Bruker
         mi = /MI=(.*)cm, MAXI/.match(c)[1]
         mx = /MAXI=(.*)cm, PC/.match(c)[1]
         pc = /PC=(.*)/.match(c)[1]
-        PickDetails.new(f1, f2, mi, mx, pc)
-        return PickDetails.new(f1, f2, mi, mx, pc)
+        PeakPickDetails.new(f1, f2, mi, mx, pc)
+        return PeakPickDetails.new(f1, f2, mi, mx, pc)
       end
 
       # Parses and wraps a group of peak lists
       # ARGS
       #    xmlDoc : The file path of the xml document to parse
       # RETURNS
-      #    A list of new PeakList objects built from the contents of xmlDoc
+      #    A list of new PeakList1D objects built from the contents of xmlDoc
       def parsePL(xmlDoc)
         @doc = Nokogiri::XML::Document.parse(File.open(xmlDoc))
 
         ls = []
         hs = []
-        @doc.css("PeakList1D").each do |l_node|
-          h = l_node.at_css("PeakList1DHeader")
-          pd = h.at_css("PeakPickDetails")
-          c = h.at_css("@creator")
+        @doc.css("PeakList1D1D").each do |l_node|
+          h = l_node.at_css("PeakList1D1DHeader")
+          pd = h.at_css("PeakPeakPickDetails")
+          c = h.at_css("@creator").value.to_s
           d = h.at_css("@date")
-          x = h.at_css("@expNo")
-          n = h.at_css("@name")
-          o = h.at_css("@owner")
-          p = h.at_css("@procNo")
-          s = h.at_css("@source")
-          dets = h.at_css("PeakPickDetails")
+          x = h.at_css("@expNo").value.to_i
+          n = h.at_css("@name").value.to_s
+          o = h.at_css("@owner").value.to_s
+          p = h.at_css("@procNo").value.to_i
+          s = h.at_css("@source").value.to_s
+          dets = h.at_css("PeakPeakPickDetails")
           dets = parseDetails(dets)
-          h = ListHeader.new(c, d, x, n, o, p, s, dets)
+          h = PeakList1DHeader.new(c, d, x, n, o, p, s, dets)
 
           ps = l_node.css("Peak1D").to_a
-          ps.map!{|p| Peak.new(p.at_css("@F1"), p.at_css("@intensity"), p.at_css("@type"))}
-          ls << PeakList.new(h, ps)
+          ps.map!{|p| Peak1D.new(p.at_css("@F1"), p.at_css("@intensity"), p.at_css("@type"))}
+          ls << PeakList1D.new(h, ps)
         end
 
         return ls
@@ -56,39 +56,34 @@ module Bruker
   end
 
   # Wrapper around a list of peaks and their accompanying header
-  class PeakList
+  class PeakList1D
+    attr_accessor (:header, :peaks)
      def initialize(header, peaks)
        @header = header
        @peaks = peaks
      end
-
-     def display
-       puts "#{@header.display}\n"
-       @peaks.each {|elem| puts "#{elem.display}\n"}
-       puts "------------------------------------------------------------\n"
-     end
   end
 
   # Wrapper around a peak list's header
-  class ListHeader
+  class PeakList1D1DHeader
+    attr_accessor (:creator, :date, :expNo, :name, :owner, :procNo, :source, :details)
+
     def initialize(creator, date, expNo, name, owner, procNo, source, details)
       @creator = creator
       @date = DateTime.strptime(date, "%Y-%m-%dT%H:%M:%S")
-      @expNo = expNo.value.to_i
+      @expNo = expNo
       @name = name
       @owner = owner
-      @procNo = procNo.value.to_i
+      @procNo = procNo
       @source = source
       @details = details
-    end
-
-    def display
-      puts "Creator: #{@creator}\nDate: #{@date}\nExpNo: #{@expNo}\nName: #{@name}\nOwner: #{@owner}\nProcNo: #{@procNo}\nSource: #{@source}\n#{@details.display}"
     end
   end
 
   # Wrapper around the parameters for peak selection
-  class PickDetails
+  class PeakPickDetails
+    attr_accessor(:f1, :f2, :MI, :MAXI, :PC)
+
     def initialize(f1, f2, mi, maxi, pc)
       @F1 = f1
       @F2 = f2
@@ -96,22 +91,16 @@ module Bruker
       @MAXI = maxi
       @PC = pc
     end
-
-    def display
-      puts "F1: #{@F1}\tF2: #{@F2}\tMI: #{@MI}\tMAXI: #{@MAXI}\tPC: #{@PC}"
-    end
   end
 
   # Wrapper around individual peak data
-  class Peak
+  class Peak1D
+    attr_accessor (:f1, :intensity, :type)
+
     def initialize(f1, intensity, type)
       @f1 = f1
       @intensity = intensity
       @type = type
-    end
-
-    def display
-      puts "F1: #{@f1}\nIntensity: #{@intensity}\nType: #{@type}"
     end
   end
 end
