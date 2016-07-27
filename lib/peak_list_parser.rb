c70709f471cf7851ee096aefa09004fad18e7af7
Scott Howland <thydungeonmaster@gmail.com>
2016-07-27 10:11:54 -0700

    Casting
    Migrated nokogiri node casting from data struct constructors to the parser itself.

--- lib/peak_list_parser.rb
+++ lib/peak_list_parser.rb
@@ -12,11 +12,11 @@ module Bruker
       #    A new PeakPickDetails object built from ds
       def parseDetails(ds)
         c = ds.content
-        f1 = /F1=(.*)ppm, F2/.match(c)[1]
-        f2 = /F2=(.*)ppm, MI/.match(c)[1]
-        mi = /MI=(.*)cm, MAXI/.match(c)[1]
-        mx = /MAXI=(.*)cm, PC/.match(c)[1]
-        pc = /PC=(.*)/.match(c)[1]
+        f1 = /F1=(.*)ppm, F2/.match(c)[1].to_f
+        f2 = /F2=(.*)ppm, MI/.match(c)[1].to_f
+        mi = /MI=(.*)cm, MAXI/.match(c)[1].to_f
+        mx = /MAXI=(.*)cm, PC/.match(c)[1].to_f
+        pc = /PC=(.*)/.match(c)[1].to_f
         return PeakPickDetails.new(f1, f2, mi, mx, pc)
       end
 
@@ -32,19 +32,20 @@ module Bruker
         hs = []
         @doc.css("PeakList1D").each do |l_node|
           h = l_node.at_css("PeakList1DHeader")
-          c = h.at_css("@creator")
+          c = h.at_css("@creator").value.to_s
           d = h.at_css("@date")
-          x = h.at_css("@expNo")
-          n = h.at_css("@name")
-          o = h.at_css("@owner")
-          p = h.at_css("@procNo")
-          s = h.at_css("@source")
+          d = DateTime.strptime(d, "%Y-%m-%dT%H:%M:%S")
+          x = h.at_css("@expNo").value.to_i
+          n = h.at_css("@name").value.to_s
+          o = h.at_css("@owner").value.to_s
+          p = h.at_css("@procNo").value.to_i
+          s = h.at_css("@source").value.to_s
           dets = h.at_css("PeakPickDetails")
           dets = parseDetails(dets)
           h = PeakList1DHeader.new(c, d, x, n, o, p, s, dets)
 
           ps = l_node.css("Peak1D").to_a
-          ps.map!{|p| Peak1D.new(p.at_css("@F1"), p.at_css("@intensity"), p.at_css("@type"))}
+          ps.map!{|p| Peak1D.new(p.at_css("@F1").value.to_f, p.at_css("@intensity").value.to_f, p.at_css("@type").value.to_i)}
           ls << PeakList1D.new(h, ps)
         end
 
@@ -67,13 +68,13 @@ module Bruker
     attr_accessor :creator, :date, :expNo, :name, :owner, :procNo, :source, :details
 
     def initialize(creator, date, expNo, name, owner, procNo, source, details)
-      @creator = creator.value.to_s
-      @date = DateTime.strptime(date, "%Y-%m-%dT%H:%M:%S")
-      @expNo = expNo.value.to_i
-      @name = name.value.to_s
-      @owner = owner.value.to_s
-      @procNo = procNo.value.to_i
-      @source = source.value.to_s
+      @creator = creator
+      @date = date
+      @expNo = expNo
+      @name = name
+      @owner = owner
+      @procNo = procNo
+      @source = source
       @details = details
     end
   end
@@ -83,11 +84,11 @@ module Bruker
     attr_accessor :f1, :f2, :MI, :MAXI, :PC
 
     def initialize(f1, f2, mi, maxi, pc)
-      @F1 = f1.to_f
-      @F2 = f2.to_f
-      @MI = mi.to_f
-      @MAXI = maxi.to_f
-      @PC = pc.to_f
+      @F1 = f1
+      @F2 = f2
+      @MI = mi
+      @MAXI = maxi
+      @PC = pc
     end
   end
 
@@ -96,12 +97,12 @@ module Bruker
     attr_accessor :f1, :intensity, :type
 
     def initialize(f1, intensity, type)
-      @f1 = f1.value.to_f
-      @intensity = intensity.value.to_f
-      @type = type.value.to_i
+      @f1 = f1
+      @intensity = intensity
+      @type = type
     end
   end
 end
 
-ls = Bruker::XML::PLParser.new.parsePL(ARGV[0])
-puts ls
+# ls = Bruker::XML::PLParser.new.parsePL(ARGV[0])
+# puts ls
